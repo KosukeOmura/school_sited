fa268cca5309f7f69fab03b97318c4eae4ce289b
KosukeOmura <social.666.k@gmail.com>
2020-07-30 19:22:06 +0900

    no message

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+'use strict'
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('hamburger-menu').addEventListener('click', function () {
+        this.classList.toggle('active');
+        document.getElementById('nav').classList.toggle('active');
+        document.getElementById('mask').classList.toggle('active');
+    })
+});
